7b72b88fa27fdf9452a9ca2ebd7b116412a84d68
Fenix2.0 <97191482+fenix237@users.noreply.github.com>
2022-12-31 07:47:28 +0100

    Add files via upload
    La page des parametres

---
+++ inf3075_mobile_money/lib/pages/liste.dart
@@ -0,0 +1,53 @@
+// ignore: unused_import
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+
+class Liste extends StatelessWidget {
+  const Liste({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ListView(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Image.asset("assets/images/theme.png"),
+                  title: const Text("Changer Mode"),
+                  subtitle: const Text("theme sombre,theme clair"),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Image.asset("assets/images/phone.png"),
+                  title: const Text("Changer Numero"),
+                  subtitle:
+                      const Text("veuillez changer votre numero de telephone"),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Image.asset("assets/images/langue2.png"),
+                  title: const Text("Changer Langue"),
+                  subtitle: const Text("francais,anglais,.."),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Image.asset("assets/images/help.png"),
+                  title: const Text("Aide"),
+                  subtitle: const Text("Centre d'aide, contactez nous"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
